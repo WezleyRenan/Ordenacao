ef6930b97828e189fff7d4562df00aece0440826
Wess <118078373+WezleyRenan@users.noreply.github.com>
2023-04-12 12:10:06 -0300

    Add files via upload

---
+++ QuickSort/Quick.java
@@ -0,0 +1,52 @@
+package QuickSort;
+
+public class Quick {
+
+	public static void main(String[] args) {
+
+		int[] vetor = { 2, 6, 3, 5, 1, 4 };
+
+		System.out.println("Desordenado");
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+		System.out.println(" ");
+		System.out.println("ordenado");
+		quicksort(vetor,0,vetor.length-1);
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+
+		/// quicksort///
+
+	}
+
+	static void quicksort(int[] vetor, int esquerda, int direita) {
+		if (esquerda < direita) {
+			int p = particao(vetor, esquerda, direita);
+			quicksort(vetor,esquerda,p);
+			quicksort(vetor,p + 1,direita);
+		}
+	}
+
+	static int particao(int[] vetor, int esquerda, int direita) {
+		int meio = (int) (esquerda + direita) / 2;
+		int pivot = vetor[meio];
+		int i = esquerda - 1;
+		int j = direita + 1;
+		while (true) {
+			do {
+				i++;
+			} while (vetor[i] < pivot);
+			do {
+				j--;
+			} while (vetor[j] > pivot);
+			if (i >= j) {
+				return j;
+			}
+			int aux = vetor[i];
+			vetor[i] = vetor[j];
+			vetor[j] = aux;
+		}
+	}
+}
